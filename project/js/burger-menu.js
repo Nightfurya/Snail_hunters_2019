e4730decf67bfccd20d25bf0d7d515ce81f23398
Nightfurya <y.m.logvinenko@gmail.com>
2019-08-06 19:49:54 +0300

    Burger-menu.js - fixed; html nav-routes - fixed

--- project/js/burger-menu.js
+++ project/js/burger-menu.js
@@ -1,17 +1,17 @@
 $(document).on("DOMContentLoaded", () => {
-  let cross = document.querySelector(".burger-menu__cross");
-  let burgerMenu = document.querySelector(".navigation__media");
-  let burgerMenuOption = document.querySelector(".navigation__media-li");
+  let cross = $(".burger-menu__cross");
+  let burgerMenu = $(".navigation__media");
+  let burgerMenuOption = $(".navigation__media-li");
 
   // кнопка бургер меню
-  cross.addEventListener("click", () => {
-    cross.classList.toggle("open");
-    burgerMenu.classList.toggle("hidden");
+  cross.on("click", () => {
+    cross.toggleClass("open");
+    burgerMenu.toggleClass("hidden");
   });
 
   // скрытие элемента burgerMenu по клику на одну из опций
-  burgerMenuOption.addEventListener("click", () => {
-    cross.classList.toggle("open");
-    burgerMenu.classList.toggle("hidden");
+  burgerMenuOption.on("click", () => {
+    cross.toggleClass("open");
+    burgerMenu.toggleClass("hidden");
   });
 });
